c2d7d37e9e8c9206d640bb2a48762f3bc60b49b2
quyentx <quyentx@outlook.com>
2016-02-17 13:11:33 +0700

    Edit some methods in quyentx's package

--- src/com/tsc/seleniumex/quyentx/NewTour.java
+++ src/com/tsc/seleniumex/quyentx/NewTour.java
@@ -16,10 +16,12 @@ public class NewTour {
 //		checkSignUp();
 //		
 //	}
+
 	@Test
-	public void checkLogIn(){
-		String expectedTitle = "Welcome: Mercury Tours";
+	public void checkLogInValidCred(){
+		String expectedTitle = "Sign-on: Mercury Tours";
         String actualTitle = "";
+		driver.get("http://newtours.demoaut.com/mercurysignon.php");
 		
 		actualTitle = driver.getTitle();
 		if(actualTitle.contentEquals(expectedTitle)){
@@ -29,20 +31,53 @@ public class NewTour {
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[1]/td[2]/input")).sendKeys("tutorial");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input")).sendKeys("tutorial");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input")).click();
+			
+			String currentTitle = "";
+			currentTitle = driver.getTitle();
+			if(currentTitle == "Find a flight: Mercury Tours"){
+				System.out.println("PASS: Loggin successfully with VALID credentials");
+			}
 		}else{
 			System.out.println("False title!");
 		}
-		
-		
 	}
+	
 	@Test
-	public void checkSignUp(){
+	public void checkLogInInvalidCred(){
+		String expectedTitle = "Sign-on: Mercury Tours";
+        String actualTitle = "";
+		driver.get("http://newtours.demoaut.com/mercurysignon.php");
+		
+		actualTitle = driver.getTitle();
+		if(actualTitle.contentEquals(expectedTitle)){
+			
+			System.out.println("Correct title! Logging in with INVALID credentials...");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a")).click();
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[1]/td[2]/input")).sendKeys("tutorial");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input")).sendKeys("password");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input")).click();
+			System.out.println("Test finished with INVALID credentials");
+
+			String currentTitle = "";
+			currentTitle = driver.getTitle();
+			if(currentTitle == "Sign-on: Mercury Tours"){
+				System.out.println("PASS: Loggin successfully with INVALID credentials");
+			}
+		}else{
+			System.out.println("False title!");
+		}	
+	}
+	
+	@Test
+	public void checkRegisterValidInfo(){
 		String expectedTitle = "Register: Mercury Tours";
         String actualTitle = "";
 		driver.get("http://newtours.demoaut.com/mercuryregister.php");
 		
 		actualTitle = driver.getTitle();
 		if(actualTitle.contentEquals(expectedTitle)){
+			System.out.println("Correct title. Registering with INVALID info...");
+			
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input")).sendKeys("Quyen");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Ta");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/input")).sendKeys("0966997385");
@@ -55,7 +90,49 @@ public class NewTour {
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input")).sendKeys("password1");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[16]/td[2]/input")).sendKeys("password1");
 			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[18]/td/input")).click();
+			
+			String currentURL = "";
+			currentURL = driver.getCurrentUrl();
+			if(currentURL == "http://newtours.demoaut.com/create_account_success.php"){
+				System.out.println("PASS: Test finished with VALID info");
+			}
+			
+		}else{
+			System.out.println("False title!");
 		}
 	}
 	
+	@Test
+	public void checkRegisterInvalidInfo() throws InterruptedException {
+		String expectedTitle = "Register: Mercury Tours";
+        String actualTitle = "";
+		driver.get("http://newtours.demoaut.com/mercuryregister.php");
+		
+		actualTitle = driver.getTitle();
+		if(actualTitle.contentEquals(expectedTitle)){
+			System.out.println("Correct title. Registering with INVALID info...");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input")).sendKeys("Quyen");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Ta");
+			// input special characters in {Phone number} field 
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/input")).sendKeys("096699@#!$@");
+			driver.findElement(By.xpath(".//*[@id='userName']")).sendKeys("username1");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[7]/td[2]/input")).sendKeys("Address 1");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/input")).sendKeys("Tuliem");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/input")).sendKeys("Hanoi");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/input")).sendKeys("720110");
+			driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("quyentx");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input")).sendKeys("password1");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[16]/td[2]/input")).sendKeys("password1");
+			driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[18]/td/input")).click();
+			
+			String currentURL = "";
+			currentURL = driver.getCurrentUrl();
+			if(currentURL == "http://newtours.demoaut.com/create_account_unsuccess.php"){
+				System.out.println("PASS: Registered UNsuccessful with INVALID info");
+			}
+		}else{
+			System.out.println("False title!");
+		}
+
+	}	
 }
